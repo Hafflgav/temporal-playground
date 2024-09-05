e1ebb643ffd09a46e2cc48587cadbad9fabeaba8
Thomas Heinrichs <thomas.heinrichs@miragon.io>
2024-09-05 20:00:39 +0200

    Init project

---
+++ src/main/java/io/miragon/temporalplay/TemporalplayApplication.java
@@ -0,0 +1,13 @@
+package io.miragon.temporalplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemporalplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemporalplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/miragon/temporalplay/TemporalplayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.miragon.temporalplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemporalplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
